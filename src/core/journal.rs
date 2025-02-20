4987e12bb41444f0e809749871ae71c88c5ba514
William Lempinen <wlempin@gmail.com>
2025-02-20 12:23:52 +0200

    thread debud

--- src/core/journal.rs
+++ src/core/journal.rs
@@ -3,7 +3,7 @@ use log::{error, info};
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
-    thread::JoinHandle,
+    thread,
 };
 
 use tokio::{process::Command, sync::mpsc};
@@ -29,13 +29,20 @@ pub async fn get_journal_logs(service: &str) -> Result<SharedJournalLogs> {
     info!("get_journal_logs called");
 
     for p in 1..=7 {
-        info!("get_journal_logs called, in for loop");
+        info!("In loop with priority: {}", p);
         let thread_logs = logs_for_service.clone();
         let thread_service = service.to_string();
         let thread_sender = sender.clone();
 
         tokio::spawn(async move {
-            info!("Spawned with {}", p);
+            let thread_id = thread::current().id();
+            let binding = thread::current();
+            let thread_name = binding.name().unwrap_or("unknown");
+
+            info!(
+                "[TASK] Running on thread wtith priority of: {}: {:?} ({})",
+                p, thread_id, thread_name
+            );
 
             let logs = get_logs(thread_service, p)
                 .await
@@ -46,11 +53,15 @@ pub async fn get_journal_logs(service: &str) -> Result<SharedJournalLogs> {
                 .map_err(|e| RounalError::JournalCtlError(format!("{:?}", e)))
                 .ok()
                 .map(|mut logs_map| logs_map.insert(p, logs));
-            info!("Done");
 
             if let Err(e) = thread_sender.send(()).await {
                 error!("Error in thread sender: {:?}", e);
             }
+
+            info!(
+                "[TASK] ENDING on thread with priority of: {}: {:?} ({})",
+                p, thread_id, thread_name
+            );
         });
     }
 
@@ -63,12 +74,10 @@ pub async fn get_journal_logs(service: &str) -> Result<SharedJournalLogs> {
         }
     }
 
-    info!("LOGS: {:?}", logs_for_service);
     Ok(logs_for_service)
 }
 
 async fn get_logs(service: String, priority: u8) -> Result<Vec<JournalLog>> {
-    //info!("get_logs with {} {}", service, priority);
     let out = Command::new("sudo")
         .arg("journalctl")
         .arg("-u")
@@ -88,30 +97,20 @@ async fn get_logs(service: String, priority: u8) -> Result<Vec<JournalLog>> {
     }
 
     let stdout = String::from_utf8_lossy(&out.stdout);
-    //info!(
-    //    "RUN THIS: journalctl -u {:?} -r -p {:?}",
-    //    &service, &priority
-    //);
-    //info!("STDOUT: {:?}", stdout);
-
     let logs: Vec<JournalLog> = stdout
         .lines()
         .skip(1)
         .filter_map(|line| parse_log(line, &priority))
         .collect();
 
-    //info!("LOHS THEN: {:?}", logs);
-
     Ok(logs)
 }
 
 fn parse_log(log_line: &str, p: &u8) -> Option<JournalLog> {
-    //info!("LOG LINE: {:?}", log_line);
     let parts: Vec<&str> = log_line.split_whitespace().collect();
 
     match p {
         1..=7 => {
-            //info!("Found entry with priority of {}", p);
             let priority = p.clone();
             let timestamp = parts.get(..3)?.join(" ");
             let hostname = parts.get(3)?.to_string();
